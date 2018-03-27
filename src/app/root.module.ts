a55af14fae1d234f76378dce98a570185efbf3eb
pigeon Hu <Tiange.Hu@otto.de>
2018-03-27 22:29:20 +0200

    tp: init component

--- src/app/root.module.ts
+++ src/app/root.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
